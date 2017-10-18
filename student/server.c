28b95c6c92c94250a9fe198cd601d7aeda9d4e28
Rebecca Chapin-Ridgely <rchapin@terpmail.umd.edu>
2017-10-17 21:33:13 -0400

    more

--- student/server.c
+++ student/server.c
@@ -11,6 +11,11 @@
 #include "common.h"
 
 #define SPACE " "
+#define DOT "."
+#define HELLO "HELLO"
+#define STATUS "STATUS"
+#define SERVER_BYE "SERVER_BYE"
+#define CLIENT_BYE "CLIENT_BYE"
 
 //Basic socket level code refrenced from TCP-IP Sockets in C.
 
@@ -20,12 +25,14 @@ int main (int argc, char **argv){
   int serverSocket;
   int clientSocket;
   int recievedMessageSize;
+  int cookie;
   struct sockaddr_in serverAddress = {0};
   struct sockaddr_in clientAddress = {0};
   unsigned short serverPort;
   unsigned int clientLength;
   char buffer[MAX_STR_SIZE];
   char *token;
+  char *clientIP;
 
   //Check right number of inputs
   if (argc < 2){
@@ -60,8 +67,6 @@ int main (int argc, char **argv){
   clientLength = sizeof(clientAddress);
   //Infinite loop while the server waits
   for(;;){
-
-
     //Waiting for the client to connect
     if ((clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientLength)) < 0){
       perror("Accept failed");
@@ -74,12 +79,54 @@ int main (int argc, char **argv){
       exit(1);
     }
 
+    clientIP = inet_ntoa(clientAddress.sin_addr);
+
     token = strtok(buffer, SPACE);
     if (strcmp(token, MAGIC_STRING) != 0){
-      printf("**Error** from %s:%d", inet_ntoa(clientAddress.sin_addr), clientAddress.sin_port);
+      printf("**Error** from %s:%d\n", clientIP, clientAddress.sin_port);
       close(clientSocket);
+      fflush(stdout);
     }
-    printf("Message: %s", buffer);
+
+    token = strtok(buffer, SPACE);
+    if (strcmp(token, HELLO) != 0){
+      printf("**Error** from %s:%d\n", inet_ntoa(clientAddress.sin_addr), clientAddress.sin_port);
+      close(clientSocket);
+      fflush(stdout);
+    }
+
+    //Not enforced
+    token = strtok(buffer, SPACE);
+    if (!token){
+      printf("**Error** from %s:%d\n", inet_ntoa(clientAddress.sin_addr), clientAddress.sin_port);
+      close(clientSocket);
+      fflush(stdout);
+    }
+    token = strtok(buffer, SPACE);
+    if (!token){
+      printf("**Error** from %s:%d\n", inet_ntoa(clientAddress.sin_addr), clientAddress.sin_port);
+      close(clientSocket);
+      fflush(stdout);
+    }
+
+    token = strtok(buffer, SPACE);
+    if (token){
+      printf("**Error** from %s:%d\n", inet_ntoa(clientAddress.sin_addr), clientAddress.sin_port);
+      close(clientSocket);
+      fflush(stdout);
+    }
+
+    cookie = (atoi(strtok(clientIP, DOT)) + atoi(strtok(clientIP, DOT)) + atoi(strtok(clientIP, DOT)) + atoi(strtok(clientIP, DOT)))*13 % 1111;
+    snprintf(buffer, sizeof(buffer), "%s %s %d %s:%d", MAGIC_STRING, STATUS, cookie, clientIP, clientAddress.sin_port);
+    print(buffer);
+    fflush(stdout);
+
+    if (send(clientSocket, buffer, sizeof(buffer),0) != sizeof(buffer)){
+      perror("Send Failure");
+      exit(1);
+    }
+
+    printf("Message: %s\n", buffer);
 
   }
 
